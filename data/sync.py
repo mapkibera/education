0efcf93697ed7bf59f1d9a29c7d0fd9a855c1023
Mikel Maron <mikel_maron@yahoo.com>
2014-04-20 09:38:00 -0400

    Update sync.py

--- data/sync.py
+++ data/sync.py
@@ -3,19 +3,23 @@ import urllib, urllib2
 import base64
 import csv
 import os
+import geojson
 
 north = -1.3000
 south = -1.3232
 east = 36.8079
 west = 36.7663
 
-def sync():
+def sync_osm():
   url = 'http://overpass-api.de/api/interpreter'
+  file_name = 'kibera-schools-osm.xml'
   values = dict(data='<osm-script><osm-script output="json" timeout="25"><union><query type="node"><has-kv k="amenity" v="school"/><bbox-query e="36.8079" n="-1.3000" s="-1.3232" w="36.7663"/></query></union><print mode="body"/><recurse type="down"/><print mode="skeleton" order="quadtile"/></osm-script></osm-script>')  
   data = urllib.urlencode(values)
   req = urllib2.Request(url, data)
   rsp = urllib2.urlopen(req)
-  print rsp.read()
+  myFile = open(file_name, 'w')
+  myFile.write(rsp.read())
+  myFile.close()
 
 def url2file(url,file_name):
   req = urllib2.Request(url)
@@ -54,7 +58,39 @@ def filter_kenyaopendata():
 def convert2geojson():
   os.system("ogr2ogr -f GeoJSON kibera-primary-schools.geojson kibera-primary-schools.vrt")
   os.system("ogr2ogr -f GeoJSON kibera-secondary-schools.geojson kibera-secondary-schools.vrt")
+  os.system("osmtogeojson kibera-schools-osm.xml > kibera-schools-osm.geojson")
 
-kenyaopendata()
-filter_kenyaopendata()
-convert2geojson()
+def readfile(filename):
+  with open(filename, 'r') as f:
+    read_data = f.read()
+  f.closed
+  return read_data
+
+def writefile(file_name, buf):
+  myFile = open(file_name, 'w')
+  myFile.write(buf)
+  myFile.close()
+
+def compare_osm_kenyaopendata():
+  osm = geojson.loads(readfile('kibera-schools-osm.geojson'))
+  kod = geojson.loads(readfile('kibera-primary-schools.geojson'))
+  result = {}
+  result['type'] = 'FeatureCollection'
+  result['features'] = []
+
+  for feature in osm.features:
+    if 'official_name' in feature.properties:
+      for kod_feature in kod.features:
+        if 'official_name' in kod_feature.properties and kod_feature.properties['official_name'] == feature.properties['official_name']:
+          #print feature.properties['official_name']
+          geom = "MultiPoint([(" + str(feature.geometry.coordinates[0]) + "," + str(feature.geometry.coordinates[1]) + "),(" + str(kod_feature.geometry.coordinates[0]) + "," + str(kod_feature.geometry.coordinates[1]) + ")])"
+          result['features'].append( { "type": "Feature", "properties": feature.properties, "geometry": geom })
+
+  dump = geojson.dumps(result)
+  writefile('kibera-combined-schools.geojson',dump)
+  
+#kenyaopendata()
+#filter_kenyaopendata()
+#convert2geojson()
+#sync_osm()
+compare_osm_kenyaopendata()
